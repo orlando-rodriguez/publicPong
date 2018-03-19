11b5ba5adf05ab675712836f5f71ba3aed8fecdc
Orlando Rodriguez <orlando.r@me.com>
2018-03-19 15:47:53 -0600

    submit

--- drills/version-1/scripts/app.js
+++ drills/version-1/scripts/app.js
@@ -1,1 +1,37 @@
-//your code here
+
+const url = 'https://quiet-bayou-99554.herokuapp.com/api/v1/contacts'
+const charactersList = document.querySelector('#characters')
+
+fetch(url)
+    .then((response) => response.json())
+    .then((characters) => {
+      let characterArray = characters.data
+      characterArray.map(function (character) {
+        let li = createNode('li'),
+            img = createNode('img'),
+            p = createNode('p'),
+            a = createNode('a'),
+            span = createNode('span')
+            img.src = character.imageURL
+            a.textContent = `Leave ${character.name} a message`
+            a.href = `contact.html?character=${character.name}`
+            span.textContent = `${character.name} ${character.phone}`
+            p.textContent = `${character.message}`
+            appendNode(charactersList, li)
+            appendNode(li, img)
+            appendNode(li, span)
+            appendNode(li, a)
+            appendNode(li, p)
+      })
+    })
+    .catch(function(err) {
+      console.log(err.message);
+    });
+
+function createNode (element) {
+  return document.createElement(element)
+}
+
+function appendNode (parent, element) {
+  return parent.appendChild(element)
+}
--- drills/version-1/scripts/form.js
+++ drills/version-1/scripts/form.js
@@ -1,1 +1,30 @@
-// your code here
+let char = document.getElementById('character');
+char.value = document.location.search.split("=")[1]
+
+
+const url = 'https://quiet-bayou-99554.herokuapp.com/api/v1/contacts'
+const button = document.querySelector('.submit')
+
+button.addEventListener('click', function(event){
+  event.preventDefault()
+  fetch(url, {
+    method: "POST",
+    headers: new Headers({
+      "content-type":
+      "application/json"
+    }),
+    body: JSON.stringify({
+      "data": {
+        "character": "Joyce",
+        "message": "Chill out."
+        }
+    })
+  })
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(response) {
+    console.log(response)
+  })
+
+})
